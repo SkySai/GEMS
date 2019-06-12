bc99b678f7d08d80318f3848abec300469158dd8
Vincent He <vincent.dexter.he@gmail.com>
2019-06-12 12:03:56 -0500

    Add files via upload

--- template.sh
+++ template.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 count=1
-myArr=$(<yourCommandHere> | awk '{for (i=1; i<=NF; i++) print $i}') #your command goes here#
+myArr=()
+gshell='Sysmond.getSystemServicesHealth'  ## your gshell command here
+delimiter=","                             ## the delimiter you want to use to parse the text into tokens
+myArr=$(gshell -c "$gshell" -c exit | awk -F "$delimiter" '{for (i=1; i<=NF; i++) print $i}') 
 for i in ${myArr[@]}
 do 
  printf $count
